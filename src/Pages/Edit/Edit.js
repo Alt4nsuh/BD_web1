c161c6d2f48ea3d045c0e0df78c7ab2650a55e28
Tuvshinjarga1 <ttuvshuu40@yahoo.com>
2023-12-06 13:43:11 +0800

    edit dotor hi gj bichle

--- src/Pages/Edit/Edit.js
+++ src/Pages/Edit/Edit.js
@@ -1,11 +1,10 @@
 // Header.js
 import React from 'react';
-import { Link } from 'react-router-dom';
 function Header() {
   return (
     <div className='head'>
       <img src='C:\A_Hicheel\Laravel\bd_web\src\Image\logo.png' alt='Logo' width='200' height='100' />
-      
+      <p>higg</p>
     </div>
   );
 }
