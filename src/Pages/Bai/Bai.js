554d20dd9cf142d0d83550f8dc8b1e59ae39b053
Alt4nsuh <batzorigaltansh@gmail.com>
2023-12-10 13:32:25 +0800

    Router zassan

--- src/Pages/Bai/Bai.js
+++ src/Pages/Bai/Bai.js
@@ -1,36 +1,40 @@
-import React from 'react';
-import './Bai.css';
-import { Link } from 'react-router-dom';
-import Header from '../Header/Header';
+import React from "react";
+import "./Bai.css";
+import { Link } from "react-router-dom";
+import Header from "../Header/Header";
 function Bai() {
   return (
     <div className="mainBai">
-      <Header/>
+      <Header />
 
-      <div className='secHead'>
-        <a href='#TetEronhii'>Тэтгэлгийн <br></br>ерөнхий <br></br>мэдээлэл</a>
-        <a href='#TetShaardlaga'>Тэтгэлэгт <br></br>тавигдах <br></br>шаардлага</a>
-        <a href='#TetMateral'>Бүрдүүлэх <br></br>материал</a>
-        <a href='#TetHolboo'>Холбоо <br></br>барих</a>
+      <div className="secHead">
+        <a href="#TetEronhii">
+          Тэтгэлгийн <br></br>ерөнхий <br></br>мэдээлэл
+        </a>
+        <a href="#TetShaardlaga">
+          Тэтгэлэгт <br></br>тавигдах <br></br>шаардлага
+        </a>
+        <a href="#TetMateral">
+          Бүрдүүлэх <br></br>материал
+        </a>
+        <a href="#TetHolboo">
+          Холбоо <br></br>барих
+        </a>
       </div>
 
-      <div className='BaiBody'>
-        <div id='TetEronhii' className='TetEronhii'>
-        <h1>Тэтгэлгийн 
-ерөнхий
- мэдээлэл</h1>
+      <div className="BaiBody">
+        <div id="TetEronhii" className="TetEronhii">
+          <h1>Тэтгэлгийн ерөнхий мэдээлэл</h1>
+          
         </div>
-        <div id='TetShaardlaga' className='TetShaardlaga'>
-        <h1>Тэтгэлэгт 
-тавигдах 
-шаардлага</h1>
+        <div id="TetShaardlaga" className="TetShaardlaga">
+          <h1>Тэтгэлэгт тавигдах шаардлага</h1>
         </div>
-        <div id='TetMateral' className='TetMateral'>
-        <h1>Бүрдүүлэх 
-материал</h1>
+        <div id="TetMateral" className="TetMateral">
+          <h1>Бүрдүүлэх материал</h1>
         </div>
-        <div id='TetHolboo' className='TetHolboo'>
-        <h1>Холбоо барих</h1>
+        <div id="TetHolboo" className="TetHolboo">
+          <h1>Холбоо барих</h1>
         </div>
       </div>
     </div>
--- src/Router/MainRoutes.js
+++ src/Router/MainRoutes.js
@@ -1,3 +1,4 @@
+// MainRoutes.js
 import React from 'react';
 import { useRoutes } from 'react-router-dom';
 import Bai from '../Pages/Bai/Bai';
@@ -5,16 +6,20 @@ import BaiHuvi from '../Pages/BaiHuvi/BaiHuvi';
 import Edit from '../Pages/Edit/Edit';
 import Login from '../Pages/Login/Login';
 import Sign from '../Pages/Sign/Sign';
-import Home from '../Pages/Home/Home';
 
 export const MainRoutes = () => {
-    return useRoutes([
-        { path: '/', element: <Login /> },
-        { path: '/sign', element: <Sign /> },
-        { path: '/Bai', element: <Bai /> },
-        { path: '/BaiHuvi', element: <BaiHuvi /> },
-        { path: '/BaiHuvi/edit', element: <Edit /> },
-
-
-    ]);
+  return useRoutes([
+    { path: '/', element: <Login /> },
+    { path: '/sign', element: <Sign /> },
+    { path: '/Bai', element: <Bai /> },
+    {
+      path: '/BaiHuvi',
+      element: <BaiHuvi />,
+      children: [
+        { path: 'edit', element: <Edit /> },
+      ],
+    },
+    { path: '/Home', element: <Home /> },
+    { path: '/Tetgeleg', element: <Tetgeleg /> },
+  ]);
 };
